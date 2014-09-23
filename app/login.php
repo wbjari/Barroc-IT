4d57b1794eff7ab0b8d8db74389dc787bd08471e
Cliff Sestig <cliffsestig@gmail.com>
2014-09-23 16:14:32 +0200

    Update

--- app/login.php
+++ app/login.php
@@ -6,4 +6,22 @@ require '../config/config.php';
 
 
 ?>
+<div class='container'>
+	<form method="post" action="controllers/authController.php" role="form" class="login-form col-md-4 col-md-offset-4">
+		<fieldset>
+			<legend><h2>Login</h2></legend>
+			<div class='form-group'>
+				<label for='username'> Username </label>
+				<input type='username'name='username' id='username' class='form-control'>
+			</div>
+			<div class='form-group'>
+				<label for='password'> Password </label>
+				<input type='password'name='password' id='password' class='form-control'>
+			</div>
+			<div class='form-group'>
+				<input type='submit'name='authUser' id='Login' class='btn btn-large'>
+			</div>
+		</fieldset>
+	</form>
+</div>
 <? require 'templates/footer.php';?>
--- app/templates/header.php
+++ app/templates/header.php
@@ -5,7 +5,6 @@
 	<meta charset="UTF-8">
 	<title>Barroc IT</title>
 	<link rel="stylesheet" href="http://barroc.radiusdev.nl/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 	
