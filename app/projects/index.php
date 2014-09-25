88d4df93b64598385ff763de1836ed67aae6cfe1
Quincy Soeliman <qsoeliman@gmail.com>
2014-09-25 09:04:13 +0200

    Update

---
+++ app/projects/index.php
@@ -0,0 +1,36 @@
+<?php require '../templates/header.php'; 
+	require '../../config/config.php';	
+?>
+<div class="container">
+	<div class="row">
+		<h3>Projects</h3>
+	</div>
+	<div class="row">
+		<table class="table table-striped table-bordered">
+			<thread>
+				<tr>
+					<th>Project ID</th>
+					<th>Project name</th>
+					<th>Costumer</th>
+					<th>Email</th>
+				</tr>
+			</thread>
+			<tbody>
+			<?php
+				$result = mysqli_query($con, 'SELECT * FROM projects ORDER BY id ASC');
+
+				while($row = mysqli_fetch_array($result)) {
+					echo '<tr>';
+					echo '<td>'. $row['id'] . '</td>';
+					echo '<td>'. $row['projectname'] . '</td>';
+					echo '<td>'. $row['customer'] . '</td>';
+					echo '<td>'. $row['email'] . '</td>';
+					echo '</tr>';
+				}
+
+				mysqli_close($con);
+			?>
+			</tbody>
+		</table>
+	</div>
+</div>
