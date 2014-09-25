76017c492f5b2bc6e0167026455ec9c1fed5548f
wbjari <wbjari@gmail.com>
2014-09-25 12:06:49 +0200

    dingen.

--- app/templates/header.php
+++ app/templates/header.php
@@ -7,8 +7,11 @@
 	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 </head>
-<body>
+<body>		
 	<div class="container">
-		<div class="page-header">
-			<img src="../img/banner.png" class="banner" />
+		<div class="page-header">		
+				<div class="banner">
+					<h1>BARROC IT.</h1>
+					<h1>SOFTWARE FOR REAL.</h1>
+				</div>
 		</div>
