a0d04aa55ee0f15ce18de251ef0e1f24504a99d4
Cliff Sestig <cliffsestig@gmail.com>
2014-09-25 09:35:04 +0200

    Connectie db terug naar local

--- config/config.php
+++ config/config.php
@@ -1,10 +1,5 @@
 <?php
 
-$con = mysqli_connect('localhost', 'sitetestin_grp3', 'Hg7XfzXf3X', 'sitetestin_grp3');
-
-if($con = true)
-{
-echo 'hallo';
-}
+$con = mysqli_connect('localhost', 'root', '', 'barrocit');
 
 ?>
