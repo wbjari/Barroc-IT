ba9654b5bb41519f7f9dd477fb5461b8da37af51
Cliff Sestig <cliffsestig@gmail.com>
2014-09-25 09:29:42 +0200

    update

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root', '', 'barrocit');
+$con = mysqli_connect('localhost', 'sitetestin_grp3', 'Hg7XfzXf3X', 'sitetestin_grp3');
+
+if($con = true)
+{
+echo 'hallo';
+}
 
 ?>
