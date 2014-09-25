015274eefce39a662a3f5fb184a6d91f5ce89610
Cliff Sestig <cliffsestig@gmail.com>
2014-09-25 11:15:45 +0200

    invoice controller and finance aangemaakt

---
+++ app/controllers/invoiceController.php
@@ -0,0 +1,10 @@
+<?php 	require '../../config/config.php';?>
+<?php
+
+$sql = 'SELECT * FROM invoices';
+$result = mysqli_query($con, $sql);
+
+
+
+
+?>
---
+++ app/finance/index.php
@@ -0,0 +1,28 @@
+<?php 	require '../templates/header.php';
+		require '../controllers/invoiceController.php';
+?>
+
+<table class='table table-striped'>
+		<thead>
+			<tr>
+				<td class="col-sm-2">Customername</td>
+				<td class="col-sm-2">Customerproject</td>
+			</tr>
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+		  while ($row = mysqli_fetch_assoc($result)) 
+        {
+         echo '<tr>';
+         echo '<td>' . $row['Customername'] . '</td>';
+         echo '<td>' . $row['Customerproject'] . '</td>';
+         echo '<td> <a href="index.php?id=' . $row['id'] . '"</a> Change </td>';
+         echo '</tr>';        
+        }       
+		?>
+		</tbody>
+</table>
+
+
+<?php require'../templates/footer.php';?>
