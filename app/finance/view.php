c518c79d9670fb10ac1c5696c77b45d24ca88436
Cliff Sestig <cliffsestig@gmail.com>
2014-09-26 11:43:37 +0200

    Update

--- app/finance/view.php
+++ app/finance/view.php
@@ -17,7 +17,12 @@ if(isset($_GET['id']))
     <h1>Finance panel: View</h1>
 </div>
 <div class='form-group'>
+  <div class='float_btn'>
 	<input type='submit'name='authController' id='Logout' class='btn btn-large' value='Logout'>
+  </div>
+</div>
+<div class='form-group'>
+  <a class='btn btn-success' href='edit.php'>Edit</a>
 </div>
 <!-- <ul class="list-unstyled">
 <li><a href="invoices.php">Invoices</a></li>
@@ -27,33 +32,24 @@ if(isset($_GET['id']))
  <table class='table table-striped'>
     <thead>
       <tr>
-      <?php
-      while ($row = mysqli_fetch_assoc($r_view)) 
-        {
-          echo '<tr>';
-          ?>
         <td class="col-sm-2">Customername</td>
-        <?php echo '<td>' . $row['Customername'] . '</td>';?>
         <td class="col-sm-2">Customerproject</td>
-         <?php echo '<td>' . $row['Customerproject'] . '</td>';?>
-         <td class="col-sm-2">Customername</td>
-        <?php echo '<td>' . $row['Customername'] . '</td>';?>
-        <td class="col-sm-2">Customerproject</td>
-         <?php echo '<td>' . $row['Customerproject'] . '</td>';?>
-         <td class="col-sm-2">Customername</td>
-        <?php echo '<td>' . $row['Customername'] . '</td>';?>
-        <td class="col-sm-2">Customerproject</td>
-         <?php echo '<td>' . $row['Customerproject'] . '</td>';?>
-
       </tr>
       </tr>
     </thead>
     <tbody>
     <?php
+      while ($row = mysqli_fetch_assoc($r_view)) 
+        {
+         echo '<tr>';
+         echo '<td>' . $row['Customername'] . '</td>';
+         echo '<td>' . $row['Customerproject'] . '</td>';
          echo '</tr>';        
         }       
     ?>
 </table>
 
-
+<div class='form-group'>
+  <a class='btn btn-success' href='index.php'>Back</a>
+</div>
 <?php require'../templates/footer.php';?>
